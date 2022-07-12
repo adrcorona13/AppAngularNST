645ddf1b41ef5b96b4a4b1b66f9d17238de5043a
Adrian Pineda <adrianpinedacorona@hotmail.com>
2022-07-12 11:23:10 -0500

    stop timer when finishing game

--- src/app/Components/timer/timer.component.ts
+++ src/app/Components/timer/timer.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnInit, OnDestroy {
 
   @Input()
   timeLeft: number|any = 5;
   interval: any;
 
   constructor() { }
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 
   ngOnInit(): void {
     this.startTimer();
